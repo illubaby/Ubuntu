aea2fcb501236e90ca652a7de845724e8fead9f4
i_am_illubaby ! <aironb177@gmail.com>
2024-02-28 13:30:34 +0700

    change

--- app_and_customize.sh
+++ app_and_customize.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Prepresite
+# sudo apt update
+# sudo apt upgrade
+# sudo apt-get update
+
+# App
+# sudo apt install gnome-tweaks
+# sudo apt-get install git
+# sudo apt install gcc
+
+# Setting
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.desktop.interface scaling-factor 1
