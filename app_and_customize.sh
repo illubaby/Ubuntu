77323ac2115c2d10788f4278586ee382aa49fe43
i_am_illubaby ! <aironb177@gmail.com>
2024-02-28 14:06:58 +0700

    change

--- app_and_customize.sh
+++ app_and_customize.sh
@@ -11,5 +11,8 @@
 # sudo apt install gcc
 
 # Setting
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-gsettings set org.gnome.desktop.interface scaling-factor 1
+#gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+#wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1Lx1qBLfHpWyRCg_fA3fXTRLLrTsWAOTC' -O ~/Pictures/background.jpg
+#gsettings set org.gnome.desktop.background picture-uri '~/Pictures/background.jpg'
+#gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
