3e2b418345272e6fe827a8f6d92fac3e59fa6e68
i_am_illubaby ! <aironb177@gmail.com>
2024-02-28 14:16:48 +0700

    de

--- app_and_customize.sh
+++ app_and_customize.sh
@@ -15,4 +15,6 @@
 #wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1Lx1qBLfHpWyRCg_fA3fXTRLLrTsWAOTC' -O ~/Pictures/background.jpg
 #gsettings set org.gnome.desktop.background picture-uri '~/Pictures/background.jpg'
 #gsettings set org.gnome.shell.extensions.desktop-icons show-home false
-gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+#gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'RIGHT'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
