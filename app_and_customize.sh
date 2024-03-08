4323e343c9448e34e9591d25a7146354f510778a
i_am_illubaby ! <aironb177@gmail.com>
2024-03-08 10:16:52 +0700

    Update the backup file

--- app_and_customize.sh
+++ app_and_customize.sh
@@ -9,12 +9,14 @@
 # sudo apt install gnome-tweaks
 # sudo apt-get install git
 # sudo apt install gcc
-
+sudo apt install gnome-software
 # Setting
 #gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 #wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1Lx1qBLfHpWyRCg_fA3fXTRLLrTsWAOTC' -O ~/Pictures/background.jpg
 #gsettings set org.gnome.desktop.background picture-uri '~/Pictures/background.jpg'
 #gsettings set org.gnome.shell.extensions.desktop-icons show-home false
 #gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'RIGHT'
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+#gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'RIGHT'
+#gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+
+sudo apt install gnome-software
